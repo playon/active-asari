47de355fe7952cf2e45e05cf82b9f06c887aacdf
Lance Gleason <lgleasain@yahoo.com>
2013-08-01 14:27:37 -0400

    basic migration support added to get domains and elements indexed in the cloud

--- lib/active_asari/migrations.rb
+++ lib/active_asari/migrations.rb
@@ -8,20 +8,30 @@ module ActiveAsari
     end
 
     def migrate_all
+      ACTIVE_ASARI_CONFIG.keys.each do |domain|
+        migrate_domain domain
+      end
     end
 
     def migrate_domain(domain)
-            #ACTIVE_ASARI_CONFIG[class_name].symbolize_keys.each do |index|
-
-      #end
+      ACTIVE_ASARI_CONFIG[domain].each do |field|
+        create_index_field domain, field.first => field.last
+      end
     end
 
     def create_index_field(domain, field)
       index_field_name = field.keys.first
       index_field_type = field[index_field_name]['index_field_type']
-      search_enabled = (field[index_field_name]['search_enabled'].downcase.strip == 'true')
+      search_enabled = (field[index_field_name]['search_enabled'] and (field[index_field_name]['search_enabled'].downcase.strip == 'true'))
+
       request = {:domain_name => domain, :index_field => {:index_field_name => index_field_name,
-      :index_field_type => index_field_type, :literal_options => {:search_enabled => search_enabled, :result_enabled => true}}}
+      :index_field_type => index_field_type}}
+      case index_field_type 
+      when 'literal'
+        request[:index_field][:literal_options] = {:search_enabled => search_enabled, :result_enabled => true}
+      when 'text'
+        request[:index_field][:text_options] = {:result_enabled => true}
+      end
       connection.create_index_field request
     end
 
--- spec/lib/migrations_spec.rb
+++ spec/lib/migrations_spec.rb
@@ -16,16 +16,22 @@ describe 'migrations' do
         {:index_field_name => 'band_name', :index_field_type => 'literal', :literal_options =>
           {:search_enabled => false, :result_enabled => true}}}
       migrations.connection.should_receive(:create_index_field).with(expected_index_field_request).and_return CREATE_LITERAL_INDEX_RESPONSE     
-      migrations.create_index_field('beavis', 'band_name' => { 'index_field_type' => 'literal', 'search_enabled' => 'false'})
-      
+      migrations.create_index_field('beavis', 'band_name' => { 'index_field_type' => 'literal', 'search_enabled' => false})
     end
 
     it 'should add a text index to the domain' do
       expected_index_field_request = {:domain_name => 'beavis', :index_field => 
-        {:index_field_name => 'band_name', :index_field_type => 'literal', :literal_options =>
-          {:search_enabled => false, :result_enabled => true}}}
-      
-      
+        {:index_field_name => 'tv_location', :index_field_type => 'text', :text_options =>
+          {:result_enabled => true}}}
+      migrations.connection.should_receive(:create_index_field).with(expected_index_field_request).and_return CREATE_TEXT_INDEX_RESPONSE 
+      migrations.create_index_field('beavis', 'tv_location' => { 'index_field_type' => 'text'})
+    end
+
+    it 'should add a uint index to the domain' do
+      expected_index_field_request = {:domain_name => 'beavis', :index_field => 
+        {:index_field_name => 'num_tvs', :index_field_type => 'uint'}}
+      migrations.connection.should_receive(:create_index_field).with(expected_index_field_request).and_return CREATE_UINT_INDEX_RESPONSE 
+      migrations.create_index_field('beavis', 'num_tvs' => { 'index_field_type' => 'uint'})
     end
   end
 
@@ -36,6 +42,27 @@ describe 'migrations' do
       migrations.create_domain 'beavis'
     end
 
+    it 'should create indexes for all items in the domain' do
+      migrations.should_receive(:create_index_field).once.with('TestModel', 
+                                                               'name' => { 'index_field_type' => 'text', 
+                                                                 'search_enabled' => true})
+      migrations.should_receive(:create_index_field).once.with('TestModel', 
+                                                               'amount' => { 'index_field_type' => 'uint', 
+                                                                 'search_enabled' => true})
+      migrations.should_receive(:create_index_field).once.with('TestModel', 
+                                                               'last_updated' => { 'index_field_type' => 'uint', 
+                                                                 'search_enabled' => false})
+
+      migrations.migrate_domain 'TestModel'
+    end
+  end
+
+  context 'migrate_all' do
+    it 'should migrate all of the domains' do
+      migrations.should_receive(:migrate_domain).once.with 'TestModel'
+      migrations.should_receive(:migrate_domain).once.with 'HoneyBadger'
+      migrations.migrate_all
+    end
   end
 
 end
